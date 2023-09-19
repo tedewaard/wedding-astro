3c6f67a63f2d6c0842b9460c3d3fbe90c2f39ac4
Trevor Edewaard <tr.edewaard@gmail.com>
2023-09-19 18:51:37 -0400

    Working on styling/aliging the form for RSVP

--- src/components/rsvp.tsx
+++ src/components/rsvp.tsx
@@ -22,24 +22,35 @@ export default function Rsvp({guests}) {
     //TODO: If the guest has already RSVPd then let them know
 
     //TODO: After submitting the RSVP show a message
-    //TODO: Add the option to choose a vegan or vegetarian meal
+    //TODO: Add the option to choose a vegan or vegetarian meal. Do this after the RSVP maybe? 
 
     return(
-        <div>
-            <h3 className="text-lg font-bold text-center">Will you be attending?</h3>
-            <form onSubmit={submit}>
-            {guests.map((data, idx) => {
-                return (
-                    <div key={idx} className="flex m-1">
-                        <label htmlFor="rsvpSelect" className="mx-5 w-1/2" >{data.Name}</label>
-                        <select name={data.Name} id="rsvpSelect" className="">
-                            <option value="yes">Yes</option>
-                            <option value="no">No</option>
-                        </select>
-                    </div>
-                )})}
-                <button id="submit" className="block mb-4 mx-auto w-1/3 text-center border-black border rounded-lg" type="submit" value="Submit">Submit</button>
+        <div className="">
+            <form id="rsvp" className="flex">
+                <div>
+                    <label className="text-sm mb-4 whitespace-nowrap block">Will you attend?</label>
+                    <label className="text-sm whitespace-nowrap block">Dietary Restrictions?</label>
+                </div>
+                {guests.map((data, idx) => {
+                    return(
+                        <div key={idx}>
+                            <label htmlFor="rsvpSelect" className="whitespace-nowrap">{data.Name}</label>
+                            <select name={data.Name} id="rsvpSelect" className="block w-full my-4">
+                                <option value="yes">Yes</option>
+                                <option value="no">No</option>
+                            </select>
+                            <select name={data.Name} id="rsvpSelect" className="block w-full">
+                                <option value="none">None</option>
+                                <option value="vegetarian">Vegetarian</option>
+                                <option value="vegan">Vegan</option>
+                            </select>
+                        </div>
+                    )
+                })}
             </form>
+                <div className="mt-4">
+                    <button id="submit" form="rsvp" className="block mb-4 mx-auto w-1/3 text-center border-black border rounded-lg" type="submit" value="Submit">Submit</button>
+                </div>
        </div> 
     )
 }
@@ -49,3 +60,27 @@ export default function Rsvp({guests}) {
                     <p key={idx}>Guest: {data.Name}</p>
                 )})}
 */
+/*
+            <div className="flex justify-between">
+                <h3 className="text-sm font-bold ml-5">Will you be attending?</h3>
+                <h3 className=" text-sm font-bold">Dietary Restrictions?</h3>
+            </div>
+            <form onSubmit={submit}>
+            {guests.map((data, idx) => {
+                return (
+                    <div key={idx} className="flex m-1">
+                        <label htmlFor="rsvpSelect" className="px-5 w-1/2" >{data.Name}</label>
+                        <div className="flex">                        
+                            <select name={data.Name} id="rsvpSelect" className="w-1/3">
+                                <option value="yes">Yes</option>
+                                <option value="no">No</option>
+                            </select>
+                            <select name={data.Name} id="rsvpSelect" className="pl-4 w-2/3">
+                                <option value="none">None</option>
+                                <option value="vegetarian">Vegetarian</option>
+                                <option value="vegan">Vegan</option>
+                            </select>
+                        </div>
+                    </div>
+                )})}
+                */
